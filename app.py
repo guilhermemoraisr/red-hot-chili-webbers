ec6698ed505e2cd23179386e476fcd57b1a8d35e
IanFPFerreira <ianfillipe@gmail.com>
2019-06-18 22:59:12 -0300

    :tada: commit inicial

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = 'pipoca'
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+
+@app.route('/presentation_data')
+def present():
+    return render_template('fim.html', renda=float(session['income']))
+#-------------------------------------fazer este comando caso queira fazer um 'if/else' no arquivo fim.html
+
+@app.route('/form', methods=['POST'])
+def create_information():
+    session['income'] = request.form['income']
+    session['ethnicity'] = request.form.get('ethnicity')
+    session['gender'] = request.form.get('gender')
+    session['age'] = request.form['age']
+    session['state'] = request.form['state']
+    session['graduation'] = request.form['graduation']
+    return redirect(url_for('present'))
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ models/form.py
@@ -0,0 +1,9 @@
+
+class Form(object):
+    def __init__(self, income, ethnicity, gender, age, state, graduation):
+        self.income = income
+        self.ethnicity = ethnicity
+        self.gender = gender
+        self.age = age
+        self.state = state
+        self.graduation = graduation
