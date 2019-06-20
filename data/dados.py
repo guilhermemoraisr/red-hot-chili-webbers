0019b52d985714ee2b509cb092f85e664bf5148b
IanFPFerreira <ianfillipe@gmail.com>
2019-06-20 00:51:09 -0300

    Update dados.py

--- data/dados.py
+++ data/dados.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 #%%
 url = 'https://raw.githubusercontent.com/fabiommendes/desenvolvimento-de-software/master/dados/pnad2012.csv'
-df = pd.read_csv(url, index_col=0)
+df = pd.read_csv(url, index_col=0, sep=',')
 df.mean()
 
 #%%
@@ -22,7 +22,7 @@ df_gen = df.groupby('gender').mean()
 df_gen[['income', 'income_work', 'income_rent', 'income_capital']].plot.line()
 
 #%%
-df_race = df.groupby('gender').mean()
+df_race = df.groupby('race').mean()
 
 #%%
 df_race[['income', 'income_work', 'income_rent', 'income_capital']].plot.line()
@@ -36,32 +36,20 @@ df_edu[['income', 'income_work', 'income_rent', 'income_capital']].min()
 #%%
 df_edu[['income', 'income_work', 'income_rent', 'income_capital']].mean()
 
+#%%
+df.groupby(['gender', 'race']).max()
 
 #%%
-df.groupby('gender').max()
+df.groupby(['gender', 'race']).min()
 
 #%%
-df.groupby('gender').min()
+df.groupby(['gender', 'race']).mean()
 
 #%%
-df.groupby('gender').mean()
+df.groupby(['gender', 'education']).max()
 
 #%%
-df.groupby('race').max()
+df.groupby(['gender', 'education']).max()
 
 #%%
-df.groupby('race').min()
-
-#%%
-df.groupby('race').mean()
-
-#%%
-df.groupby('education').max()
-
-#%%
-df.groupby('education').min()
-
-#%%
-df.groupby('education').mean()
-
-
+df.groupby(['gender', 'education']).max()
