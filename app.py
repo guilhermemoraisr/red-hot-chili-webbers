a31c809204c08310019fbd9c4eda13570eecb720
italovinicius18 <39673124+italovinicius18@users.noreply.github.com>
2019-07-01 19:53:20 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'pipoca'
-df = ...
+#df = ...
 
 @app.route('/')
 def home_page():
