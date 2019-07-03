cd5f1fc3ea755eec26309eb64f6036410c1ecfa0
italovinicius18 <39673124+italovinicius18@users.noreply.github.com>
2019-07-03 19:55:55 -0300

    Dados atualizados

--- data/dados.py
+++ data/dados.py
@@ -2,7 +2,7 @@ import pandas as pd
 url = 'https://raw.githubusercontent.com/fabiommendes/desenvolvimento-de-software/master/dados/pnad2012.csv'
 dados = pd.read_csv(url, index_col=0, sep=',')
 
-dados2 = dados.fillna(0)
+dados2 = dados.fillna(dados.mean())
 
 dados3 = dados2.copy()
 
@@ -46,6 +46,68 @@ def Genero():
 
 def Etnia():
     return ((100 - ((100 * negros.income.mean()) / brancos.income.mean())).round(1))
+
+
+def MediaFinal(salario, raca, sexo, estado, edu):
+    rendamax = income.max()
+    rendamin = income.min()
+
+    if sexo = 1 and raca = 1:
+        privsexoraca=100
+    if sexo = 2 and raca = 1:
+        privsexoraca = 66
+    if sexo = 1 and raca = 2:
+        privsexoraca=33
+    if sexo = 2 and raca = 2:
+        privsexoraca=0
+    
+    if edu = 1:
+        privedu = 14
+    if edu = 2:
+        privedu = 28
+    if edu = 3:
+        privedu = 42
+    if edu = 4:
+        privedu = 56
+    if edu = 5:
+        privedu = 78
+    if edu = 6:
+        privedu = 84
+    if edu = 7:
+        privedu = 100
+    
+
+    if estado == 1 or estado == 3 or estado == 4 or estado == 14 or estado == 22 or estado == 23 or estado == 27 :
+        privestado = 25
+    #<!--Acre, Amapá, Amazonas, Pará, Rondônia, Roraima, Tocantins: Região Norte-->
+    elif estado == 2 or estado == 5 or estado == 6 or estado == 10 or estado == 15 or estado == 17 or estado == 18 or estado == 20 or estado == 26 :
+        privestado = 0
+    #<!--Alagoas, Bahia, Ceará, Maranhão, Paraíba, Pernambuco, Rio Grande do Norte, Sergipe: Região Nordeste-->
+    elif estado == 7 or estado == 9 or estado == 11 or estado == 12 :
+        privestado = 75
+    #<!--Distrito Federal, Goiás, Mato Grosso, Mato Grosso do Sul: Região Centro-Oeste-->
+    elif estado == 8 or estado == 13 or estado == 19 or estado == 25 :
+        privestado = 100
+    #<!--Espírito Santo, Minas Gerais, Rio de Janeiro, São Paulo: Região Sudeste-->
+    elif estado == 16 or estado == 21 or estado == 24 :
+        privestado = 50
+    #<!--Paraná, Rio Grande do Sul, Santa Catarina: Região Sul-->
+
+
+    if salario > rendamax:
+        salario = rendamax
+    if salario < rendamin:
+        salario = rendamin
+    
+    diferenca = rendamax-rendamin
+
+    privrenda = (salario*100)/diferenca
+
+    privfinal = (privrenda+privedu+privestado+privsexoraca)/4
+
+    
+
+
 
 
 """
@@ -161,6 +223,4 @@ negros.income.mean()
 
 #%%
 ((100 - ((100 * sem_estudo.income.mean()) / sup_completo.income.mean())) + (100 - ((100 * fund_incompleto.income.mean()) / sup_completo.income.mean())) + (100 - ((100 * fund_completo.income.mean()) / sup_completo.income.mean())) + (100 - ((100 * med_incompleto.income.mean()) / sup_completo.income.mean())) + (100 - ((100 * med_completo.income.mean()) / sup_completo.income.mean())) + (100 - ((100 * sup_incompleto.income.mean()) / sup_completo.income.mean()))) / 6
-
-#%%
 """
